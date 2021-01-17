708cdbd9ce06eb065ef6119f1bf550fecdb85d20
anamariabd <anabuenahoradiaz@gmail.com>
2021-01-16 21:20:21 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {loginComponent} from './components/login/login.component'
+
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    loginComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule, 
-    MatButtonModule, MatFormFieldModule, MatInputModule
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+    MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
